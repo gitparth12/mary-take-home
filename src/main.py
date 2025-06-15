aa61d9c17fa72ad15ccf14732e20021210d110e4
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-06-16 00:44:07 +1000

    Run gemini with credits, hit rate limits still

--- src/main.py
+++ src/main.py
@@ -10,11 +10,11 @@ from helpers import *
 _ = load_dotenv()
 
 def main():
-    # gemini = Gemini()
-    # gemini.initialise_chat(build_prompt())
-    mistral = Mistral()
-    mistral.initialise_chat(build_prompt())
-    generate_from_all_pages(mistral.generate, "data/document_ocr.txt", "data/dataset.jsonl")
+    gemini = Gemini()
+    gemini.initialise_chat(build_prompt())
+    # mistral = Mistral()
+    # mistral.initialise_chat(build_prompt())
+    generate_from_all_pages(gemini.generate, "data/document_ocr.txt", "data/dataset.jsonl")
 
 if __name__ == "__main__":
     main()
